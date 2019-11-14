0246d077a85726d96b5c2770909d45bcbe80672e
Chrisc <crabtree1@email.arizona.edu>
2019-11-13 17:14:52 -0700

    New Print

--- src/Connect4.java
+++ src/Connect4.java
@@ -4,6 +4,7 @@ public class Connect4 {
 	
 	public static void main(String[] args) {
 		System.out.print("Hello World");
+		System.out.print("Hello World1");
 		 Application.launch(Connect4View.class, args);
 	}
 }
