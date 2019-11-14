12c42cb9834b31e1397afa7c67ca88c26b452819
Chrisc <crabtree1@email.arizona.edu>
2019-11-13 17:09:57 -0700

    Added Print Statement

--- src/Connect4.java
+++ src/Connect4.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 public class Connect4 {
 	
 	public static void main(String[] args) {
+		System.out.print("Hello World");
 		 Application.launch(Connect4View.class, args);
 	}
 }
