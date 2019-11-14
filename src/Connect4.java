42c9ea869d229c5e0ea4d91d1f3667554f66b612
Luke <lacernetic@email.arizona.edu>
2019-11-13 22:01:15 -0700

    removed main prints

--- src/Connect4.java
+++ src/Connect4.java
@@ -3,7 +3,6 @@ import javafx.application.Application;
 public class Connect4 {
 	
 	public static void main(String[] args) {
-		System.out.print("Luke");
-		 Application.launch(Connect4View.class, args);
+		Application.launch(Connect4View.class, args);
 	}
 }
